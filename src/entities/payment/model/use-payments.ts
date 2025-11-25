a4ab1022057e4d24f6fec4ce30cd7035fdac0ebf
kidboi666 <kidboi666@gmail.com>
2025-11-25 23:52:40 +0900

    refactor: useQuery -> useSuspenseQuery 로 수정

--- src/entities/payment/model/use-payments.ts
+++ src/entities/payment/model/use-payments.ts
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { paymentQueries } from "@/entities/payment/api/queries";
 
 export function usePayments() {
-  const { data, isPending, isError, error } = useQuery(
+  const { data, isPending, isError, error } = useSuspenseQuery(
     paymentQueries.getPayments(),
   );
 
