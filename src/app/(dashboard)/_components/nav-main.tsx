de729a95541ccdca9694edf761c9cf73f7d9ec96
kidboi666 <kidboi666@gmail.com>
2025-11-25 14:46:14 +0900

    feat: 사이드바 네비게이션 버튼 컴포넌트 분리

---
+++ src/app/(dashboard)/_components/nav-main.tsx
@@ -0,0 +1,47 @@
+import type { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+interface NavMainProps {
+  items: {
+    label: string;
+    icon: LucideIcon;
+    url: string;
+  }[];
+  pathname: string;
+}
+
+export function NavMain({ items, pathname }: NavMainProps) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => {
+            const Icon = item.icon;
+            let isActive = false;
+            if (pathname === "/" && item.url === "/") {
+              isActive = true;
+            } else if (pathname === item.url) {
+              isActive = true;
+            }
+            return (
+              <SidebarMenuItem key={item.label}>
+                <SidebarMenuButton asChild isActive={isActive}>
+                  <Link href={item.url}>
+                    <Icon /> <span>{item.label}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
