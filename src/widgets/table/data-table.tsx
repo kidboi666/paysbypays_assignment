c21adf9420d4ea714347dca0b824c2409ff6217e
kidboi666 <kidboi666@gmail.com>
2025-11-26 16:22:35 +0900

    remove: 미사용 코드 삭제

--- src/widgets/table/data-table.tsx
+++ src/widgets/table/data-table.tsx
@@ -87,10 +87,6 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
         <div className="space-x-2">
           <Button
             variant="outline"
