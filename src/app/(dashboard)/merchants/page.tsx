538172a11ce4dbb057f5bc1575dac03c6737057a
kidboi666 <kidboi666@gmail.com>
2025-11-26 19:58:04 +0900

    feat: MerchantsPageView 추가

--- src/app/(dashboard)/merchants/page.tsx
+++ src/app/(dashboard)/merchants/page.tsx
@@ -1,3 +1,16 @@
-export default function PaymentsPage() {
-  return <main className="p-4 space-y-4"></main>;
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { merchantQueries } from "@/entities/merchant/api/queries";
+import { getQueryClient } from "@/shared/lib/query-client/get-query-client";
+import { MerchantsPageView } from "@/views/merchants/merchants-page-view";
+
+export default async function MerchantsPage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(merchantQueries.getMerchants());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MerchantsPageView />
+    </HydrationBoundary>
+  );
 }
