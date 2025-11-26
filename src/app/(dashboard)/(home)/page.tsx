8a9bed06a5c1ab1669631fcd2ccd65f741765147
kidboi666 <kidboi666@gmail.com>
2025-11-27 00:00:49 +0900

    remove: 미사용 view page 및 임포트 구문 삭제

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -1,16 +1,3 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { paymentQueries } from "@/entities/payment/api/queries";
-import { getQueryClient } from "@/shared/lib/query-client/get-query-client";
-import { HomePageView } from "@/views/home/home-page-view";
-
-export default async function HomePage() {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery(paymentQueries.getPayments());
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <HomePageView />
-    </HydrationBoundary>
-  );
+export default function HomePage() {
+  return <main>HomePage</main>;
 }
--- src/views/home/home-page-view.tsx
+++
@@ -1,3 +0,0 @@
-export function HomePageView() {
-  return <main className="flex flex-1 p-4"></main>;
-}
