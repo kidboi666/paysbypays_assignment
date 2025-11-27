d24ec437b03eee5b71ffcb23b1dcb0f2be55d27d
kidboi666 <kidboi666@gmail.com>
2025-11-27 18:10:38 +0900

    rename: shared/types -> shared/model 로 폴더 이름 변경

--- src/entities/merchant/api/fetchers.ts
+++ src/entities/merchant/api/fetchers.ts
@@ -1,6 +1,6 @@
 import type { Merchant } from "@/entities/merchant/model/types";
 import { axios } from "@/shared/lib/axios/axios";
-import type { BaseResponse } from "@/shared/types/api";
+import type { BaseResponse } from "@/shared/model/types";
 
 export async function getMerchants(): Promise<BaseResponse<Merchant[]>> {
   const response = await axios.get("/merchants/list");
