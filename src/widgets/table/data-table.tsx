db77cd02591595b4c76c6a0ac75e64ac5d8c5ca8
kidboi666 <kidboi666@gmail.com>
2025-11-27 13:55:23 +0900

    feat: data-table에 pagination 구현

--- src/widgets/table/data-table.tsx
+++ src/widgets/table/data-table.tsx
@@ -7,8 +7,17 @@ import {
   getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "@/shared/components/ui/button";
+import React from "react";
+import { Input } from "@/shared/components/ui/input";
+import {
+  Pagination,
+  PaginationButton,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNextButton,
+  PaginationPreviousButton,
+} from "@/shared/components/ui/pagination";
 import {
   Table,
   TableBody,
@@ -17,6 +26,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/shared/components/ui/table";
+
+const PAGE_SIZE = 17;
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -32,10 +43,23 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: PAGE_SIZE,
+      },
+    },
   });
+  const [pageInput, setPageInput] = React.useState(
+    `${table.getState().pagination.pageIndex + 1}`,
+  );
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  React.useEffect(() => {
+    setPageInput(`${table.getState().pagination.pageIndex + 1}`);
+  }, [table.getState().pagination.pageIndex]);
 
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-4">
       <div className="overflow-hidden rounded-md border">
         <Table>
           <TableHeader className="bg-muted/50">
@@ -89,26 +113,39 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <ChevronLeft />
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            <ChevronRight />
-          </Button>
-        </div>
-      </div>
+      <Pagination>
+        <PaginationContent className="gap-2">
+          <PaginationItem>
+            <PaginationPreviousButton
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            />
+          </PaginationItem>
+          <PaginationItem className="flex gap-2 items-center">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                table.setPageIndex(parseInt(pageInput, 10) - 1);
+              }}
+            >
+              <Input
+                value={pageInput}
+                onChange={(e) => setPageInput(e.target.value)}
+                className="w-12 text-center h-8"
+              />
+            </form>
+            <span className="text-muted-foreground">
+              / {table.getPageCount()}
+            </span>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNextButton
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }
