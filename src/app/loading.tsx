078b0c18ade1e6055a881aa6e7f5f09517b6d9b4
kidboi666 <kidboi666@gmail.com>
2025-11-28 14:38:31 +0900

    fix: 빌드 에러 수정
    - 존재하지 않는 size 프로퍼티 삭제

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/shared/components/ui/spinner";
 export default function Loading() {
   return (
     <div className="flex h-full w-full flex-1 items-center justify-center">
-      <Spinner size="lg" />
+      <Spinner />
     </div>
   );
 }
